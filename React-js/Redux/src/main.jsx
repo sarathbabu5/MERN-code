8a2e9bb07ea8e8ddd5c07402bc6a41a3ed49fa1d
sarathbabu <parasarathbabu5@gmail.com>
2024-11-14 20:54:26 +0530

    code

---
+++ React-js/Redux/src/main.jsx
@@ -0,0 +1,9 @@
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <Provider>
+    <App />
+  </Provider>
+);
