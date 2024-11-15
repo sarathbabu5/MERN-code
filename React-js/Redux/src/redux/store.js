8f61c18064c6066d84f38da28a32bc8ce360ec59
sarathbabu <parasarathbabu5@gmail.com>
2024-11-15 14:28:04 +0530

    update

--- React-js/Redux/src/redux/store.js
+++ React-js/Redux/src/redux/store.js
@@ -1,5 +1,9 @@
-import { combineReducers, legacy_createStore } from "redux";
+import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import { reducerFunction as counterReducer } from "./counter/reducer";
 import { themeReducer } from "./theme/reducer";
 const rootReducer = combineReducers({ counterReducer, themeReducer });
-export const store = legacy_createStore(rootReducer);
+import { composeWithDevTools } from "@redux-devtools/extension";
+export const store = legacy_createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware())
+);
